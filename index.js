a8c0fbe09f35be8e296ff2276466ab23cfa14906
Arvind Umar <arvindumar4@gmail.com>
2025-01-23 01:24:41 +0530

    initial code added

---
+++ index.js
@@ -0,0 +1,7 @@
+const findGreatest = (a, b) => {
+  return a > b;
+};
+console.log(findGreatest(10, 15));
+console.log("Answer for the above is ");
+const name = "Arvind Umar";
+console.log(`My name is ${name}`);
