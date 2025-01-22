8cda9bc2b54c88c7b2bb50380fb991b3d6bf11bf
Arvind Umar <arvindumar4@gmail.com>
2025-01-23 01:29:24 +0530

    added above 18 code

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ console.log(findGreatest(10, 15));
 console.log("Answer for the above is ");
 const name = "Arvind Umar";
 console.log(`My name is ${name}`);
+const age = 29;
+function isAbove18() {
+  return age > 18;
+}
